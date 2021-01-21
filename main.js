48e21bf49d43c56ca5b17e5d46b7bfee3357d3ef
Abhayprad Jha <abhaypradjha@gmail.com>
2021-01-21 22:06:34 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 require('dotenv').config;
 
-let mode = process.env.PRODUCTION_ENV;
 
+let mode = process.env.PRODUCTION_ENV == "dev" ? true : false;
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
@@ -18,4 +18,14 @@ function createWindow() {
 
 app.whenReady().then(createWindow, _ => console.log("Error, \"whenReady\" promise not returned"));
 
-app.on()
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
